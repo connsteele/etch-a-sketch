8942974c8fd40a0c729037ea31f1f3d93199abec
connsteele <35544211+connsteele@users.noreply.github.com>
2025-05-05 15:19:39 -0700

    16 x 16 grid is drawn into html
    Using DOM to create and insert the grid with grid.js

--- js/grid.js
+++ js/grid.js
@@ -9,12 +9,18 @@ function createGrid (count) {
     // outer loop will create new rows (parent has flex-direction column)
     // inner loop should assign into colummns (parent has flex-direction row)
 
+    // Use event delegation to improve performance!
     for (let i = 0; i < count ; i++) {
         const divRowEntry = document.createElement("div");
+        divRowEntry.setAttribute("class", "grid-row");
         // set the attribute for a new row on this element
         for (let j = 0; j < count ; j++) {
+            const divColEntry = document.createElement("div");
+            divColEntry.setAttribute("class", "grid-col");
 
+            divRowEntry.appendChild(divColEntry);
         }
+        divSketcher.appendChild(divRowEntry);
     }
 }
 
